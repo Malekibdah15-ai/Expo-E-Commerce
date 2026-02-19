cae1ccd60941c79f240e97f17ab9b2173f7880b2
Malekibdah15-ai <mibdah15@gmail,com>
2026-02-19 21:14:09 +0200

    backend setup

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
+const app = express();
+
+const __dirname = path.resolve();
+app.get('/api/data', (req, res) =>{
+    res.json({ message: 'Hello from the backend!' });
+})
+
+// deployment
+if(ENV.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../admin/dist'))); 
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../admin/dist/index.html'));
+    }); 
+}
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
+});
